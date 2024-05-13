efd0995f18a2aa030d2d4944cdcb87d8744d1bbc
Dipali.Sonawane <Dipali.Sonawane@MIT-DESK-003.Mahaitgov.in>
2024-05-13 12:42:56 +0530

    added depe

--- src/main/java/com/mahasbr/controller/DistrictMasterController.java
+++ src/main/java/com/mahasbr/controller/DistrictMasterController.java
@@ -1,12 +1,31 @@
 package com.mahasbr.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.controller.DistrictController;
+import com.example.demo.model.DistrictModel;
 import com.mahasbr.entity.DistrictMaster;
 import com.mahasbr.model.DistrictMasterModel;
 import com.mahasbr.response.MessageResponse;
@@ -25,4 +44,56 @@ public class DistrictMasterController {
 		return ResponseEntity.ok(new MessageResponse("Added successfully!", district));
 
 	}
+	private static final Logger logger = LoggerFactory.getLogger(DistrictMasterController.class);
+	private static final String CSV_FILE_LOCATION = "D:\\SBR WorkSpace\\data\\target\\Book3.xlsx";
+	
+	@GetMapping
+	public @ResponseBody List<DistrictModel> readCSV(){
+		List<DistrictModel> districts = new ArrayList<>();
+		Workbook workbook =null;
+		 Set<String> districtCodes = new HashSet<>(); // Set to store unique district codes
+		try {
+			// Creating a Workbook from an Excel file (.xls or .xlsx)
+			workbook = WorkbookFactory.create(new File(CSV_FILE_LOCATION));
+
+			// Retrieving the number of sheets in the Workbook
+			logger.info("Number of sheets: ", workbook.getNumberOfSheets());
+			// Print all sheets name
+			workbook.forEach(sheet -> {
+				logger.info(" => " + sheet.getSheetName());
+
+				// Create a DataFormatter to format and get each cell's value as String
+				DataFormatter dataFormatter = new DataFormatter();
+				//loop through all rows and columns and create Course object
+				int index = 0;
+				for(Row row : sheet) {
+					if(index++ == 0) continue;
+					DistrictModel district = new DistrictModel();
+					district.setId(dataFormatter.formatCellValue(row.getCell(0)));
+					district.setDistrictCode(dataFormatter.formatCellValue(row.getCell(1)));
+					district.setDistrictName(dataFormatter.formatCellValue(row.getCell(2)));
+					
+	 // Check if district code is already in set, if not add to list and set
+	                if (!districtCodes.contains(district.getDistrictCode())) {
+	                    districts.add(district);
+	                    districtCodes.add(district.getDistrictCode());
+	                }
+						}
+				
+				
+			});
+			
+		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
+			logger.error(e.getMessage(), e);
+		}finally {
+			try {
+				if(workbook != null) workbook.close();
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+
+		return districts;
+	}
 }
+
