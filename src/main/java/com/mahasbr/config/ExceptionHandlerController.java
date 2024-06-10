c68ed37436db6f628c4d763c75dbdb52708824f0
Sudhir6re <sshahare2018@gmail.com>
2024-06-10 22:21:16 +0530

    global exception

--- src/main/java/com/mahasbr/config/ExceptionHandlerController.java
+++ src/main/java/com/mahasbr/config/ExceptionHandlerController.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-
+/*
 @ControllerAdvice
 public class ExceptionHandlerController {
 
@@ -22,4 +23,15 @@ public class ExceptionHandlerController {
 	        mav.put("error","");
 	        return ResponseEntity.ok(mav);
 	    }
-	}
+	}*/
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        // Your exception handling logic
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    }
+}
